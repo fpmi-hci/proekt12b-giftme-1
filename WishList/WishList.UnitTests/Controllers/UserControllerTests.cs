6248beeb1495390a0811b174cb5e39f07b21b826
arzingy <arseniy_borsukov@mail.ru>
2022-10-13 16:22:19 +0300

    Add tests

---
+++ WishList/WishList.UnitTests/Controllers/UserControllerTests.cs
@@ -0,0 +1,170 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using FakeItEasy;
+using WishList.Business.IServices;
+using WishList.Business.Models;
+using WishList.Business.ModelsDto;
+using WishList.WebApi.Auth;
+using WishList.WebApi.Controllers;
+using WishList.Business.Tools;
+using System.Net;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace WishList.UnitTests.Controllers
+{
+    public class UserControllerTests
+    {
+        private static readonly UserController _userController;
+        private static readonly IAuthManager _authManager;
+
+        static UserControllerTests()
+        {
+            _authManager = A.Fake<IAuthManager>();
+
+            var fakeUserService = A.Fake<IUserService>();
+            var fakeMapper = A.Fake<IMapper>();
+            var fakeServiceProvider = A.Fake<IServiceProvider>();
+            var fakeAuthService = A.Fake<IAuthenticationService>();
+            var fakeTicket = A.Fake<AuthenticationTicket>();
+            var fakeContext = A.Fake<HttpContext>();
+
+            A.CallTo(() => fakeUserService.UpdateUser(A<string>._, A<UserModel>._))
+                 .Returns(new RequestResult { resultCode = HttpStatusCode.OK});
+            A.CallTo(() => fakeUserService.UpdatePassword(A<string>._, A<PasswordUpdateModel>._))
+                .Returns(new RequestResult());
+            A.CallTo(() => fakeMapper.Map<UserModel>(A<UserDto>._))
+                .Returns(new UserModel());
+            A.CallTo(() => _authManager.DecodeJwtToken(A<string>._))
+                .Returns(new JwtSecurityToken());
+            A.CallTo(() => fakeContext.RequestServices)
+                .Returns(fakeServiceProvider);
+            A.CallTo(() => fakeServiceProvider.GetService(typeof(IAuthenticationService)))
+                .Returns(fakeAuthService);
+            A.CallTo(() => fakeAuthService.AuthenticateAsync(fakeContext, A<string>._))
+                .Returns(AuthenticateResult.Success(fakeTicket));
+
+            _userController = new UserController(fakeUserService, fakeMapper, _authManager)
+            {
+                ControllerContext = { HttpContext = fakeContext }
+            };
+        }
+
+        [Fact]
+        public async void UpdateUser_ReturnUpdatedModel()
+        {
+            StubAuthManager(Guid.NewGuid().ToString());
+
+            var result = await _userController.UpdateUser(new UserModel());
+
+            var okResult = result as ObjectResult;
+            Xunit.Assert.NotNull(okResult);
+            Xunit.Assert.Equal((int)HttpStatusCode.OK, okResult.StatusCode);
+        }
+
+        [Fact]
+        public async void UpdateUser_ReturnNoauthorized()
+        {
+            StubAuthManager(string.Empty);
+
+            var result = await _userController.UpdateUser(new UserModel());
+
+            var unauthorizedResult = result as UnauthorizedObjectResult;
+            Xunit.Assert.NotNull(unauthorizedResult);
+            Xunit.Assert.IsType<string>(unauthorizedResult.Value);
+            Xunit.Assert.Equal("User not found.", unauthorizedResult.Value);
+        }
+
+        [Fact]
+        public async void UpdateUser_ReturnBadRequest()
+        {
+            StubAuthManager(Guid.NewGuid().ToString());
+
+            var result = await _userController.UpdateUser(null);
+
+            var badRequestResult = result as BadRequestObjectResult;
+            Xunit.Assert.NotNull(badRequestResult);
+            Xunit.Assert.IsType<string>(badRequestResult.Value);
+            Xunit.Assert.Equal((int)HttpStatusCode.BadRequest, badRequestResult.StatusCode);
+            Xunit.Assert.Equal("Not enough parameters to update user.", badRequestResult.Value);
+        }
+
+        [Fact]
+        public async void UpdatePassword_ReturnUpdatedPassword()
+        {
+            StubAuthManager(Guid.NewGuid().ToString());
+
+            var result = await _userController.UpdatePassword(new PasswordUpdateModel());
+
+            var okResult = result as ObjectResult;
+            Xunit.Assert.NotNull(okResult);
+            Xunit.Assert.Equal(0, okResult.StatusCode);
+        }
+
+        [Fact]
+        public async void UpdatePassword_ReturnBadRequest()
+        {
+            StubAuthManager(Guid.NewGuid().ToString());
+
+            var result = await _userController.UpdatePassword(null);
+
+            var badRequestResult = result as BadRequestObjectResult;
+            Xunit.Assert.NotNull(badRequestResult);
+            Xunit.Assert.IsType<string>(badRequestResult.Value);
+            Xunit.Assert.Equal("Not enough parameters to update password.", badRequestResult.Value);
+        }
+
+        [Fact]
+        public async void GetUserInfo_ReturnUserModel()
+        {
+            StubAuthManager(Guid.NewGuid().ToString());
+
+            var result = await _userController.GetUserInfo();
+
+            var okResult = result as OkObjectResult;
+            Xunit.Assert.NotNull(okResult);
+            Xunit.Assert.IsType<UserModel>(okResult.Value);
+            Xunit.Assert.NotNull(okResult.Value);
+        }
+
+        [Fact]
+        public async void GetUserInfo_ReturnBadRequest()
+        {
+            StubAuthManager(string.Empty);
+
+            var result = await _userController.GetUserInfo();
+
+            var unauthorizedResult = result as UnauthorizedObjectResult;
+            Xunit.Assert.NotNull(unauthorizedResult);
+            Xunit.Assert.IsType<string>(unauthorizedResult.Value);
+            Xunit.Assert.Equal("User not found.", unauthorizedResult.Value);
+        }
+
+
+        #region Helpers
+
+        private void StubAuthManager(string userId)
+        {
+            var jwtToken = new JwtSecurityToken(claims: new Claim[]
+            {
+                new("id", userId)
+            });
+
+            A.CallTo(() => _authManager.DecodeJwtToken(A<string>._))
+                .Returns(jwtToken);
+        }
+
+        #endregion
+    }
+}
---
+++ WishList/WishList.UnitTests/Services/UserServiceTests.cs
@@ -0,0 +1,353 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using AutoMapper;
+using FakeItEasy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Caching.Memory;
+using WishList.Business.Implementation;
+using WishList.Business.IServices;
+using WishList.Business.Models;
+using WishList.Business.ModelsDto;
+using WishList.DAL.Core.Entities;
+using Xunit;
+
+namespace WishList.UnitTests.Services
+{
+    public class UserServiceTests
+    {
+        [Fact]
+        public async void SignUp_ReturnsSuccess()
+        {
+            var fakeUserManager = A.Fake<UserManager<User>>();
+            var fakeMapper = A.Fake<IMapper>();
+            var fakeCacheService = A.Fake<ICacheService>();
+
+            A.CallTo(() => fakeMapper.Map<User>(A<UserDto>._))
+                .Returns(new User());
+            A.CallTo(() => fakeUserManager.CreateAsync(A<User>._, A<string>._))
+                .Returns(IdentityResult.Success);
+            A.CallTo(() => fakeUserManager.GetUserIdAsync(A<User>._))
+                .Returns(string.Empty);
+            A.CallTo(() => fakeUserManager.AddToRoleAsync(A<User>._, A<string>._))
+                .Returns(IdentityResult.Success);
+            var entry = A.Fake<ICacheEntry>();
+            A.CallTo(() => fakeCacheService.AddToCache(A<UserDto>._, A<string>._))
+                .DoesNothing();
+
+            var userService = new UserService(fakeUserManager, null, fakeMapper, fakeCacheService);
+            var result = await userService.SignUp("name", "email", "password");
+
+            Assert.Equal(IdentityResult.Success, result);
+        }
+
+        [Fact]
+        public void SignUp_ThrowsExceptionCreating()
+        {
+            var fakeUserManager = A.Fake<UserManager<User>>();
+            var fakeMapper = A.Fake<IMapper>();
+
+            A.CallTo(() => fakeMapper.Map<User>(A<UserDto>._))
+                .Returns(new User());
+            A.CallTo(() => fakeUserManager.CreateAsync(A<User>._, A<string>._))
+                .Returns(IdentityResult.Failed());
+
+            var userService = new UserService(fakeUserManager, null, fakeMapper, null);
+            var exception = Assert.ThrowsAsync<Exception>(() => userService.SignUp("name", "email", "password"));
+        }
+
+        [Fact]
+        public void SignUp_ThrowsExceptionAdding()
+        {
+            var fakeUserManager = A.Fake<UserManager<User>>();
+            var fakeMapper = A.Fake<IMapper>();
+
+            A.CallTo(() => fakeMapper.Map<User>(A<UserDto>._))
+                .Returns(new User());
+            A.CallTo(() => fakeUserManager.CreateAsync(A<User>._, A<string>._))
+                .Returns(IdentityResult.Success);
+            A.CallTo(() => fakeUserManager.GetUserIdAsync(A<User>._))
+                .Returns(string.Empty);
+            A.CallTo(() => fakeUserManager.AddToRoleAsync(A<User>._, A<string>._))
+                .Returns(IdentityResult.Failed());
+
+            var userService = new UserService(fakeUserManager, null, fakeMapper, null);
+            Assert.ThrowsAsync<Exception>(() => userService.SignUp("name", "email", "password"));
+        }
+
+        [Fact]
+        public async void SignIn_ReturnsUserDto()
+        {
+            var fakeUserManager = A.Fake<UserManager<User>>();
+            var fakeSignInManager = A.Fake<SignInManager<User>>();
+            var fakeMapper = A.Fake<IMapper>();
+
+            A.CallTo(() => fakeUserManager.FindByEmailAsync(A<string>._))
+                .Returns(new User());
+            A.CallTo(() => fakeSignInManager.PasswordSignInAsync(A<string>._, A<string>._, A<bool>._, A<bool>._))
+                .Returns(SignInResult.Success);
+            A.CallTo(() => fakeMapper.Map<UserDto>(A<User>._))
+                .Returns(new UserDto());
+
+            var userService = new UserService(fakeUserManager, fakeSignInManager, fakeMapper, null);
+            var result = await userService.SignIn("email", "password");
+
+            Assert.IsAssignableFrom<UserDto>(result);
+        }
+
+        [Fact]
+        public async void SignIn_ThrowsExceptionFinding()
+        {
+            var fakeUserManager = A.Fake<UserManager<User>>();
+
+            User user = null;
+            A.CallTo(() => fakeUserManager.FindByEmailAsync(A<string>._))
+                .Returns(user);
+
+            var userService = new UserService(fakeUserManager, null, null, null);
+
+            await Assert.ThrowsAsync<ArgumentException>(() => userService.SignIn("email", "password"));
+        }
+
+        [Fact]
+        public async void SignIn_ReturnsNull()
+        {
+            var fakeUserManager = A.Fake<UserManager<User>>();
+            var fakeSignInManager = A.Fake<SignInManager<User>>();
+
+            A.CallTo(() => fakeUserManager.FindByEmailAsync(A<string>._))
+                .Returns(new User());
+            A.CallTo(() => fakeSignInManager.PasswordSignInAsync(A<string>._, A<string>._, A<bool>._, A<bool>._))
+                .Returns(SignInResult.Failed);
+
+            var userService = new UserService(fakeUserManager, fakeSignInManager, null, null);
+            var result = await userService.SignIn("email", "password");
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async void GetByEmail_ReturnsUserDto()
+        {
+            var fakeUserManager = A.Fake<UserManager<User>>();
+            var fakeMapper = A.Fake<IMapper>();
+
+            A.CallTo(() => fakeUserManager.FindByEmailAsync(A<string>._))
+                .Returns(new User());
+
+            var userService = new UserService(fakeUserManager, null, fakeMapper, null);
+            var result = await userService.GetByEmail("email");
+
+            Assert.IsAssignableFrom<UserDto>(result);
+        }
+
+        [Fact]
+        public async void GetByEmail_ReturnsNull()
+        {
+            var fakeUserManager = A.Fake<UserManager<User>>();
+
+            User user = null;
+            A.CallTo(() => fakeUserManager.FindByEmailAsync(A<string>._))
+                .Returns(user);
+
+            var userService = new UserService(fakeUserManager, null, null, null);
+
+            var result = await userService.GetByEmail("email");
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async void GetById_ReturnsUserDtoFromCache()
+        {
+            var fakeCacheService = A.Fake<ICacheService>();
+
+            A.CallTo(() => fakeCacheService.Get<UserDto>(A<string>._))
+                .Returns(new UserDto());
+
+            var userService = new UserService(null, null, null, fakeCacheService);
+            var result = await userService.GetById("userId");
+
+            Assert.IsAssignableFrom<UserDto>(result);
+        }
+
+        [Fact]
+        public async void GetById_ReturnsUserDtoFromDb()
+        {
+            var fakeUserManager = A.Fake<UserManager<User>>();
+            var fakeMapper = A.Fake<IMapper>();
+            var fakeCacheService = A.Fake<ICacheService>();
+
+            UserDto userDto = null;
+            A.CallTo(() => fakeCacheService.Get<UserDto>(A<string>._))
+                .Returns(userDto);
+            A.CallTo(() => fakeUserManager.FindByIdAsync(A<string>._))
+                .Returns(new User());
+            A.CallTo(() => fakeUserManager.GetRolesAsync(A<User>._))
+                .Returns(new List<string>() { "User" });
+
+            var userService = new UserService(fakeUserManager, null, fakeMapper, fakeCacheService);
+            var result = await userService.GetById("email");
+
+            Assert.IsAssignableFrom<UserDto>(result);
+        }
+
+        [Fact]
+        public async void GetById_ThrowsException()
+        {
+            var fakeUserManager = A.Fake<UserManager<User>>();
+            var fakeCacheService = A.Fake<ICacheService>();
+
+            UserDto userDto = null;
+            A.CallTo(() => fakeCacheService.Get<UserDto>(A<string>._))
+                .Returns(userDto);
+            User user = null;
+            A.CallTo(() => fakeUserManager.FindByIdAsync(A<string>._))
+                .Returns(user);
+
+            var userService = new UserService(fakeUserManager, null, null, fakeCacheService);
+
+            await Assert.ThrowsAsync<ArgumentException>(async () => await userService.GetById("userId"));
+        }
+
+        [Fact]
+        public async void UpdateUser_ReturnsUserDto_RemovingFromCacheIncluded()
+        {
+            var fakeUserManager = A.Fake<UserManager<User>>();
+            var fakeMapper = A.Fake<IMapper>();
+            var fakeCacheService = A.Fake<ICacheService>();
+
+            var user = A.Fake<User>();
+            var userDto = A.Fake<UserDto>();
+            A.CallTo(() => fakeUserManager.FindByIdAsync(A<string>._))
+                .Returns(user);
+            A.CallTo(() => fakeMapper.Map(A<UserModel>._, A<User>._))
+                .Returns(user);
+            A.CallTo(() => fakeUserManager.UpdateAsync(A<User>._))
+                .Returns(IdentityResult.Success);
+            A.CallTo(() => fakeCacheService.Get<UserDto>(A<string>._))
+                .Returns(userDto);
+            A.CallTo(() => fakeCacheService.RemoveFromCache(A<string>._))
+                .DoesNothing();
+            A.CallTo(() => fakeMapper.Map<UserDto>(A<User>._))
+                .Returns(userDto);
+            A.CallTo(() => fakeUserManager.GetRolesAsync(A<User>._))
+                .Returns(new List<string>() { "User" });
+
+            var userService = new UserService(fakeUserManager, null, fakeMapper, fakeCacheService);
+            var result = await userService.UpdateUser("userId", new UserModel());
+
+            Assert.Equal(HttpStatusCode.OK, result.resultCode);
+        }
+
+        [Fact]
+        public async void UpdateUser_ReturnsUserDto_RemovingFromCacheNotIncluded()
+        {
+            var fakeUserManager = A.Fake<UserManager<User>>();
+            var fakeMapper = A.Fake<IMapper>();
+            var fakeCacheService = A.Fake<ICacheService>();
+
+            var user = A.Fake<User>();
+            UserDto userDto = null;
+            A.CallTo(() => fakeUserManager.FindByIdAsync(A<string>._))
+                .Returns(user);
+            A.CallTo(() => fakeMapper.Map(A<UserModel>._, A<User>._))
+                .Returns(user);
+            A.CallTo(() => fakeUserManager.UpdateAsync(A<User>._))
+                .Returns(IdentityResult.Success);
+            A.CallTo(() => fakeCacheService.Get<UserDto>(A<string>._))
+                .Returns(userDto);
+            A.CallTo(() => fakeMapper.Map<UserDto>(A<User>._))
+                .Returns(new UserDto());
+            A.CallTo(() => fakeUserManager.GetRolesAsync(A<User>._))
+                .Returns(new List<string>() { "User" });
+
+            var userService = new UserService(fakeUserManager, null, fakeMapper, fakeCacheService);
+            var result = await userService.UpdateUser("userId", new UserModel());
+
+            Assert.Equal(HttpStatusCode.OK, result.resultCode);
+        }
+
+        [Fact]
+        public async void UpdateUser_ThrowsExceptionFinding()
+        {
+            var fakeUserManager = A.Fake<UserManager<User>>();
+            User user = null;
+            A.CallTo(() => fakeUserManager.FindByIdAsync(A<string>._))
+                .Returns(user);
+
+            var userService = new UserService(fakeUserManager, null, null, null);
+            var result = await userService.UpdateUser("userId", new UserModel());
+
+            Assert.Equal(HttpStatusCode.Unauthorized, result.resultCode);
+        }
+
+        [Fact]
+        public async void UpdateUser_ThrowsExceptionUpdating()
+        {
+            var fakeUserManager = A.Fake<UserManager<User>>();
+            var fakeMapper = A.Fake<IMapper>();
+
+            var user = A.Fake<User>();
+            A.CallTo(() => fakeUserManager.FindByIdAsync(A<string>._))
+                .Returns(user);
+            A.CallTo(() => fakeMapper.Map(A<UserModel>._, A<User>._))
+                .Returns(user);
+            A.CallTo(() => fakeUserManager.UpdateAsync(A<User>._))
+                .Returns(IdentityResult.Failed());
+
+            var userService = new UserService(fakeUserManager, null, fakeMapper, null);
+            var result = await userService.UpdateUser("userId", new UserModel());
+
+            Assert.Equal(HttpStatusCode.BadRequest, result.resultCode);
+        }
+
+        [Fact]
+        public async void UpdatePassword_ReturnsSuccess()
+        {
+            var fakeUserManager = A.Fake<UserManager<User>>();
+            var user = A.Fake<User>();
+
+            A.CallTo(() => fakeUserManager.FindByIdAsync(A<string>._))
+                .Returns(user);
+            A.CallTo(() => fakeUserManager.ChangePasswordAsync(A<User>._, A<string>._, A<string>._))
+                .Returns(IdentityResult.Success);
+
+            var userService = new UserService(fakeUserManager, null, null, null);
+            var result = await userService.UpdatePassword("userId", new PasswordUpdateModel());
+
+            Assert.Equal(HttpStatusCode.OK, result.resultCode);
+        }
+
+        [Fact]
+        public async void UpdatePassword_ThrowsExceptionFinding()
+        {
+            var fakeUserManager = A.Fake<UserManager<User>>();
+            User user = null;
+
+            A.CallTo(() => fakeUserManager.FindByIdAsync(A<string>._))
+                .Returns(user);
+
+            var userService = new UserService(fakeUserManager, null, null, null);
+            var result = await userService.UpdatePassword("userId", new PasswordUpdateModel());
+
+            Assert.Equal(HttpStatusCode.Unauthorized, result.resultCode);
+        }
+
+        [Fact]
+        public async void UpdatePassword_ThrowsExceptionChanging()
+        {
+            var fakeUserManager = A.Fake<UserManager<User>>();
+            var user = A.Fake<User>();
+
+            A.CallTo(() => fakeUserManager.FindByIdAsync(A<string>._))
+                .Returns(user);
+            A.CallTo(() => fakeUserManager.ChangePasswordAsync(A<User>._, A<string>._, A<string>._))
+                .Returns(IdentityResult.Failed());
+
+            var userService = new UserService(fakeUserManager, null, null, null);
+            var result = await userService.UpdatePassword("userId", new PasswordUpdateModel());
+
+            Assert.Equal(HttpStatusCode.BadRequest, result.resultCode);
+        }
+    }
+}
